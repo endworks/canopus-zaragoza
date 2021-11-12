a7fe4d44c7792077557035ea8bbf5f6884b59381
Ender <endernull@icloud.com>
2021-11-12 13:43:28 +0100

    update host

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Transport } from '@nestjs/microservices';
 const microserviceOptions = {
   transport: Transport.TCP,
   options: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 8877,
   },
 };
