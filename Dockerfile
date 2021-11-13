3aed634369fde88602c80bd85aafb24f5a8065bb
Ender <endernull@icloud.com>
2021-11-13 04:23:23 +0100

    Add Sandbox

--- Dockerfile
+++ Dockerfile
@@ -17,12 +17,12 @@ LABEL org.opencontainers.image.source https://github.com/drkcat/canopus-zaragoza
 WORKDIR /usr/src/app
 
 COPY package.json ./
+COPY yarn.lock ./
 
 RUN yarn --prod
 
 COPY --from=builder /usr/src/app/dist ./dist
 
-EXPOSE 3001/tcp
 EXPOSE 8877/tcp
 
 CMD ["yarn", "run", "start:prod"]
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
 
-const microserviceOptions = {
-  transport: Transport.TCP,
-  options: {
-    host: '0.0.0.0',
-    port: 8877,
-  },
-};
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice(microserviceOptions, { inheritAppConfig: true });
-
-  await app.startAllMicroservices();
-  await app.listen(3001);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: 8877,
+    },
+  });
+  app.listen();
 }
 bootstrap();
