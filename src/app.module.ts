2484cf47217dd55fbb026f34191f931a46abb8ae
Ender <endernull@icloud.com>
2021-11-28 19:06:17 +0100

    missing modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { BusController } from './controllers/bus.controller';
+import { CinemaController } from './controllers/cinema.controller';
 import { TramController } from './controllers/tram.controller';
 import { BusService } from './services/bus.service';
+import { CinemaService } from './services/cinema.service';
 import { TramService } from './services/tram.service';
 
 @Module({
   imports: [HttpModule],
-  controllers: [BusController, TramController],
-  providers: [BusService, TramService],
+  controllers: [BusController, TramController, CinemaController],
+  providers: [BusService, TramService, CinemaService],
 })
 export class AppModule {}
